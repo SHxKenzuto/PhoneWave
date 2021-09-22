ed31af536232a0d2d750649e937079e4ccf5ac9d
kenzuto <gabrielepollice@gmail.com>
2021-09-22 23:48:17 +0200

    Added songs queue

--- src/phonewave.py
+++ src/phonewave.py
@@ -1,23 +1,24 @@
 import discord
-from discord import client
 from discord.ext import commands
 import urllib
 import re
 from discord import FFmpegPCMAudio
-from discord.gateway import VoiceKeepAliveHandler
 import pafy
 import asyncio
 import random
+import queue
 
 phonewaves = {}
 bot = commands.Bot(command_prefix='-')
 
 class PhoneWave:
 	def __init__(self,current_song,id):
-		self.current_song=current_song
+		self.q = queue.Queue()
+		self.q.put(current_song)
+		self.current_song = current_song
 		self.loop = False
 		self.latest_player_id = id
-	 
+		 
 def urlCreator(msg):
 	url="https://www.youtube.com/results?search_query="+msg
 	url = urllib.parse.quote_plus(url.encode("utf8"),safe=':/?=+')
@@ -31,25 +32,30 @@ def musicGenerator(url_video):
 	audio = music.getbestaudio()
 	return FFmpegPCMAudio(audio.url,**ffmpeg_options)
 
-async def player(ctx,voice_client,url_video, id):
-	musica_finale = musicGenerator(url_video)
+async def player(ctx,voice_client,id):
+	while voice_client.is_playing():
+		await asyncio.sleep(1)
+	phonewaves[ctx.guild.id].current_song = phonewaves[ctx.guild.id].q.get()
+	await ctx.send(phonewaves[ctx.guild.id].current_song)
+	musica_finale = musicGenerator(phonewaves[ctx.guild.id].current_song)
 	voice_client.play(musica_finale)
 	while voice_client.is_playing():
 		await asyncio.sleep(1)
-	if phonewaves[ctx.guild.id]!=None and phonewaves[ctx.guild.id].loop == False:
+	if phonewaves[ctx.guild.id]!=None and (phonewaves[ctx.guild.id].loop == False or phonewaves[ctx.guild.id].q.empty()) :
 		await asyncio.sleep(600)
-		if voice_client!=None and not voice_client.is_playing() and phonewaves[ctx.guild.id]!= None and phonewaves[ctx.guild.id].latest_player_id == id :
+		if voice_client!=None and not voice_client.is_playing() and phonewaves[ctx.guild.id]!= None and phonewaves[ctx.guild.id].q.empty() and phonewaves[ctx.guild.id].latest_player_id == id:
 			await voice_client.disconnect()
 			del phonewaves[ctx.guild.id]
 			phonewaves[ctx.guild.id]=None
 
 async def looper(ctx,voice_client,url_video):
-    while phonewaves[ctx.guild.id]!=None and phonewaves[ctx.guild.id].loop:
-        if not voice_client.is_playing():
-            phonewaves[ctx.guild.id].latest_player_id = random.random()
-            id = phonewaves[ctx.guild.id].latest_player_id
-            await player(ctx,voice_client,url_video,id)
-        await asyncio.sleep(1)
+	while phonewaves[ctx.guild.id]!=None and phonewaves[ctx.guild.id].loop:
+		if not voice_client.is_playing():
+			phonewaves[ctx.guild.id].q.put(url_video)
+			phonewaves[ctx.guild.id].latest_player_id = random.random()
+			id = phonewaves[ctx.guild.id].latest_player_id
+			await player(ctx,voice_client,url_video)
+		await asyncio.sleep(1)
 
 @bot.command()
 async def play(ctx,*,msg):
@@ -59,19 +65,20 @@ async def play(ctx,*,msg):
 	voice_channel = ctx.message.author.voice.channel
 	voice = discord.utils.get(ctx.guild.voice_channels,name=voice_channel.name)
 	voice_client=discord.utils.get(bot.voice_clients,guild=ctx.guild)
-	if voice_client == None or not voice_client.is_playing():
-		if voice_client == None:
-			voice_client = await voice.connect()
-		elif voice_channel != voice_client:
-			await voice_client.move_to(voice_channel)
-		if re.fullmatch(r"https:\/\/www.youtube.com\/watch\?v=\S{11}",msg) == None:
-			url_video = urlCreator(msg)
-		else:
-			url_video = msg
+	if voice_client == None:
+		voice_client = await voice.connect()
+	elif not voice_client.is_playing() and voice_channel != voice_client:
+		await voice_client.move_to(voice_channel)
+	if re.fullmatch(r"https:\/\/www.youtube.com\/watch\?v=\S{11}",msg) == None:
+		url_video = urlCreator(msg)
+	else:
+		url_video = msg
+	if ctx.guild.id not in phonewaves:
 		phonewaves[ctx.guild.id]=PhoneWave(url_video,random.random())
-		id = phonewaves[ctx.guild.id].latest_player_id
-		await ctx.send(url_video)
-		await player(ctx,voice_client,url_video,id)
+	else:
+		phonewaves[ctx.guild.id].q.put(url_video)
+	id = phonewaves[ctx.guild.id].latest_player_id
+	await player(ctx,voice_client,id)
 
 
 @bot.command()
